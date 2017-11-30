517843d828be06a5a0578422ade8c89d6605a4f8
Piotr Wilczek <piotrwilczek@gmail.com>
2017-11-30 13:52:31 +0300

    Use Pattern to speed up matching
    If we create Pattern only once and reuse it, it's much faster than our
    matching algorithm.

--- commonmark-ext-replacement/src/main/java/org/commonmark/ext/replacement/ReplacementExtension.java
+++ commonmark-ext-replacement/src/main/java/org/commonmark/ext/replacement/ReplacementExtension.java
@@ -5,6 +5,8 @@ import org.commonmark.ext.replacement.internal.ReplacementPostProcessor;
 import org.commonmark.parser.Parser;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 /**
  * Extension for replacement chars using provided map.
@@ -15,17 +17,36 @@ import java.util.Map;
  */
 public class ReplacementExtension implements Parser.ParserExtension {
     private final Map<String, String> mReplacementMap;
+    private final Pattern mKeysPattern;
 
-    private ReplacementExtension(Map<String, String> replacementMap) {
+    private ReplacementExtension(Map<String, String> replacementMap, Pattern keysPattern) {
         mReplacementMap = replacementMap;
+        mKeysPattern = keysPattern;
     }
 
     public static Extension create(Map<String, String> replacementMap) {
-        return new ReplacementExtension(replacementMap);
+        return new ReplacementExtension(replacementMap, getKeysPattern(replacementMap.keySet()));
     }
 
     @Override
     public void extend(Parser.Builder parserBuilder) {
-        parserBuilder.postProcessor(new ReplacementPostProcessor(mReplacementMap));
+        parserBuilder.postProcessor(new ReplacementPostProcessor(mReplacementMap, mKeysPattern));
+    }
+
+    private static Pattern getKeysPattern(Set<String> keys) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String key : keys) {
+            if (sb.length() == 0) {
+                sb.append("(?<=(^|\\s))(");
+            } else {
+                sb.append('|');
+            }
+            sb.append(Pattern.quote(key));
+        }
+
+        sb.append(")(?=($|\\s))");
+
+        return Pattern.compile(sb.toString());
     }
 }
